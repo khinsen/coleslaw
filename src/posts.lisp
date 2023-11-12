86762ec0d422fe4b00dfbec74b634a6063331b99
Konrad Hinsen <konrad.hinsen@fastmail.net>
2023-11-12 16:39:31 +0100

    Make post URLs Frog-compatible

--- src/posts.lisp
+++ src/posts.lisp
@@ -8,10 +8,21 @@
    (toot :initarg :toot :reader toot))
   (:default-initargs :author nil :excerpt nil :toot nil))
 
+;; This function assumes that the input files follow the naming
+;; scheme of Frog, i.e. yyyy-mm-dd-slug-derived-from-title.post.
+
+(defun basename (post)
+  (let* ((input-basename (pathname-name (content-file post)))
+         (date-part (subseq input-basename 0 10))
+         (title-part (subseq input-basename 11)))
+    (format nil "~a/~a"
+            (substitute #\/ #\- date-part)
+            title-part)))
+
 (defmethod initialize-instance :after ((object post) &key)
   (with-slots (url title author excerpt format text) object
     (let (post-content)
-      (setf url (compute-url object (slugify title))
+      (setf url (compute-url object (basename object))
             format (make-keyword (string-upcase format))
             post-content (render-text text format)
             excerpt (or excerpt
